b9aefb07fa5a2e533865bbe18157d75738f65286
Thienthien1798 <phucthien24062001@gmail.com>
2023-11-03 11:24:08 +0700

    lv3-4

---
+++ lv3/lv3.js
@@ -0,0 +1,4 @@
+//////------STRING
+let a = 'I AM THIEN THIEN';
+console.log(a);
+//////nên dùng dấu `...` trong string
---
+++ lv4/lv4.js
@@ -0,0 +1,2 @@
+////CÁC METHODS ĐƯỢC DÙNG VỚI STRING
+console.log(`I'm Thiện Thiện`)
